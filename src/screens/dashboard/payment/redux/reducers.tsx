9c92ce955df75c85f1ccac39e497629c221b4ed2
BaoNguyen1815 <baonguyendo1810@gmail.com>
2020-06-10 11:21:33 +0700

    sua loi chinh ta de test source tree thoi

--- src/screens/dashboard/payment/redux/reducers.tsx
+++ src/screens/dashboard/payment/redux/reducers.tsx
@@ -1,10 +1,10 @@
 import { handleActions } from "redux-actions";
-import { setListCustomersAction } from "./actions";
+import { setListPaymentsAction } from "./actions";
 import IActionState from "./state";
 
 export default handleActions<IActionState, any>(
   {
-    [setListCustomersAction.toString()]: (state, action) => ({
+    [setListPaymentsAction.toString()]: (state, action) => ({
       ...state,
       data: action.payload.data
     })
